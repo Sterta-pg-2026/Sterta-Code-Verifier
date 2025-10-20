47922047da10fbe3af6a5337f4a4e10aec645867
damian <damiantrowski.c@gmail.com>
2025-10-20 20:52:04 +0200

    Fix typo in logging message for submission processing completion

--- src/worker/src/worker.py
+++ src/worker/src/worker.py
@@ -374,7 +374,7 @@ def process_submission_workflow() -> bool:
 
     logger.info(f"Containers finished for submission {submission.id}")
     logger.info(f"Result for submission {submission.id}: \n\n{result}\n")
-    logger.info(f"{Ansi.BOLD.value}{NAME}{Ansi.RESET.value} has finished processing submission {submission.id}.")
+    logger.info(f"{Ansi.BOLD.value}{NAME}{Ansi.RESET.value} has finished proccessing submission {submission.id}.")
     try:
         result.debug = fetch_debug_logs(os.path.join(logs_local_path, "worker.log"))
     except Exception:
